a5145a258d1531a5248c7cf85631596493e13e7f
jonas.hall <jonas.hall@uranus.uni-freiburg.de>
2021-04-29 13:45:12 +0200

    feat: Update optimal step length to latest findings (no alpha = 0 case)

--- LCQP/GetOptimalStepLength.m
+++ LCQP/GetOptimalStepLength.m
@@ -1,6 +1,6 @@
 function [ alpha ] = GetOptimalStepLength(C, g, Rk, Q, pk, xk)
 
-% Quadratic matrix
+% Outer loop objective's Hessian
 Qk = Q + Rk*C;
 
 % Quadratic term
@@ -9,27 +9,11 @@ qk = pk'*Qk*pk;
 % Linear term
 lk = pk'*(Qk*xk + g);
 
-% Step size function along step
-meritAlongStep = @(a) a^2*qk + a*lk;
-
-if (qk <= eps)
-    % Handle not convex case
-    if (meritAlongStep(1) < meritAlongStep(0))
-        alpha = 1;
-    else
-        alpha = 0;
-    end
-elseif (lk > 0)
-    % Handle no descent case
-    alpha = 0;
-else
-    % At most take step equal to one.
+% Obtain optimal step length
+if (qk > eps && lk < 0)
     alpha = min(-lk/qk, 1);
-end
-
-% Handle no step case
-if (alpha == 0)
-    alpha = 0;
+else
+    alpha = 1;
 end
 
 end
