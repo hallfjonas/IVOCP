44e867e42625e1c7283bd67576effc44344c7e10
jonas.hall <jonas.hall@uranus.uni-freiburg.de>
2021-04-29 13:46:04 +0200

    feat: Clean up LCQP and add gradient perturbation as (non default) option

--- LCQP/LCQP.m
+++ LCQP/LCQP.m
@@ -1,13 +1,104 @@
 function [ primalSteps, dualSteps, fVals, RVals, exitflag, auxOutput ] = LCQP(Q, g, A, lb, ub, lbA, ubA, L, R, params)
-%% Is input consistent?
+%% Input Consistency
 [nx, nc, ncomp] = PerformInputConsistencyTest(Q, g, A, lb, ub, lbA, ubA, L, R);
 
-% Important for dual variables
+%% Parameter handling
+% qpOASES mode ('mpc' could be faster but less precise)
+options = qpOASES_options('default');
+if (isfield(params, 'qpOASES_options'))
+    options = params.qpOASES_options;
+end
+
+% Tolerence for complementarity violation
+complementarityTolerance = 1e-10;
+if (isfield(params, 'complementarityTolerance'))
+    complementarityTolerance = params.complementarityTolerance;
+    assert(complementarityTolerance > 0);
+end
+
+% Tolerence for stationarity violation
+stationarityTolerance = options.terminationTolerance*10;
+if (isfield(params, 'stationarityTolerance'))
+    stationarityTolerance = params.stationarityTolerance;
+    assert(stationarityTolerance > 0);
+end
+
+% Initial starting point
+xk = zeros(size(nx,1));
+if (isfield(params, 'x0'))
+    % Initialize primals
+    xk = params.x0;
+    assert(all(size(xk) == [nx, 1]));
+end
+
+% Store all steps
+storeSteps = false;
+if (isfield(params, 'storeSteps'))
+    storeSteps = params.storeSteps;
+end
+
+% Initial penalty parameter
+Rk = 1;
+if (isfield(params, 'R0'))
+    Rk = params.R0;
+    assert(Rk > 0);
+end
+
+% Solve initial QP with or without penalization
+solveZeroPenaltyFirst = true;
+if (isfield(params, 'solveZeroPenaltyFirst'))
+    solveZeroPenaltyFirst = params.solveZeroPenaltyFirst;
+    assert(islogical(solveZeroPenaltyFirst));
+end
+
+% Penalty update rule
+penaltyUpdater = @(R) 2*R;
+if (isfield(params, 'penaltyUpdater'))
+    penaltyUpdater = params.penaltyUpdater;
+end
+
+% Penalty parameter value required to break the iteration.
+Rbreak = 1000000;
+if (isfield(params, 'Rbreak'))
+    Rbreak = params.Rbreak;
+    assert(Rbreak > 0);
+end
+
+% Maximum number of total iterations.
+maxIter = 100000;
+if (isfield(params, 'maxIter'))
+    maxIter = params.maxIter;
+    assert(maxIter > 0);
+end
+
+% Print output
+printStats = false;
+if (isfield(params, 'printStats'))
+    printStats = params.printStats;
+    assert(islogical(printStats));
+end
+
+% Switch to sparse matrices
+useSparseMatrices = false;
+if (isfield(params, 'useSparseMatrices'))
+    useSparseMatrices = params.useSparseMatrices;
+    assert(islogical(useSparseMatrices));
+end
+
+useGradientPerturbation = false;
+if (isfield(params, 'useGradientPerturbation'))
+    useGradientPerturbation = params.useGradientPerturbation;
+    assert(islogical(useGradientPerturbation));
+end
+
+%% Data preperation
+
+% Append rows of L and R to A
 A = [A; L; R];
 lbA = [lbA; zeros(2*ncomp,1)];
 ubA = [ubA; inf(2*ncomp,1)];
 
-%% Add unbounded box constraints if none are passed
+% Add unbounded box constraints if none are passed
 if (isempty(lb))
     lb = -inf(nx,1);
 end
@@ -15,98 +106,7 @@ if (isempty(ub))
     ub = inf(nx,1);
 end
 
-%% Parameter handling
-% qpOASES mode
-options = qpOASES_options('default');
-if (isfield(params, 'qpOASES_options'))
-    options = params.qpOASES_options;
-end
-
-% Tolerence for convergence criterion
-complementarityTolerance = 1e-10;
-if (isfield(params, 'complementarityTolerance'))
-    complementarityTolerance = params.complementarityTolerance;
-end
-
-% Tolerence for convergence criterion
-stationarityTolerance = options.terminationTolerance*10;
-if (isfield(params, 'stationarityTolerance'))
-    stationarityTolerance = params.stationarityTolerance;
-end
-
-xk = zeros(size(nx,1));
-% Initial starting point
-if (isfield(params, 'x0'))
-    % Initialize primals
-    xk = params.x0;
-else
-    warning("No initial point given.");
-end
-
-useInitializationStruct = false;
-% Initialization struct
-if (isfield(params, 'initializationStruct'))
-    initializationStruct = params.initializationStruct;
-    useInitializationStruct = true;
-    
-    if (length(initializationStruct.guessedWorkingSetC) == nc)
-        tmp = initializationStruct.guessedWorkingSetC;
-        initializationStruct.guessedWorkingSetC = [tmp; zeros(2*ncomp,1)];
-    end
-end
-
-solveZeroPenaltyFirst = true;
-% Solve initial QP with or without penalization
-if (isfield(params, 'solveZeroPenaltyFirst'))
-    solveZeroPenaltyFirst = params.solveZeroPenaltyFirst;
-end
-
-% Store all steps?
-storeSteps = false;
-if (isfield(params, 'storeSteps'))
-    storeSteps = params.storeSteps;
-end
-
-% Initial penalty parameter (check if given else 0)
-Rk = 1;
-if (isfield(params, 'R0'))
-    Rk = params.R0;
-end
-
-% Update function for penalty parameter
-penaltyUpdater = @(R) 2*R;
-if (isfield(params, 'penaltyUpdater'))
-    penaltyUpdater = params.penaltyUpdater;
-end
-
-Rbreak = 1000000;
-% Penalty parameter value required to break the iteration.
-if (isfield(params, 'Rbreak'))
-    Rbreak = params.Rbreak;
-end
-
-maxIter = 100000;
-% Penalty parameter value required to break the iteration.
-if (isfield(params, 'maxIter'))
-    maxIter = params.maxIter;
-end
-
-options.maxIter = maxIter;
-
-printStats = false;
-% Initial starting point
-if (isfield(params, 'printStats'))
-    printStats = params.printStats;
-    assert(islogical(printStats));
-end
-
-useSparseMatrices = false;
-% Initial starting point
-if (isfield(params, 'useSparseMatrices'))
-    useSparseMatrices = params.useSparseMatrices;
-    assert(islogical(useSparseMatrices));
-end
-
+% Switch to sparse matrices
 if (useSparseMatrices)
     Q = sparse(Q);
     A = sparse(A);
@@ -114,12 +114,15 @@ if (useSparseMatrices)
     R = sparse(R);
 end
 
-%% Helpers
-% global iter counter
-iter = 0;
-
-% Update iteration whenever x is updated
+%% Helpers and initializations
+% Outer loop index
 k = 0;
+
+% Inner loop index
+i = 0;
+
+% Total iterate counter
+iter = 0;
 
 % Originial objective
 F = @(primal) 1/2*primal'*Q*primal + g'*primal;
@@ -150,19 +153,21 @@ else
     [QP,xk,~,exitflag,iterQPO,lk,auxOutput] = qpOASES_sequence('i', Q, g + Rk*grad_phi(xk), A, lb, ub, lbA, ubA, options);
 end
     
+% return on failed QP solve
 if (exitflag ~= 0)
     qpOASES_sequence('c', QP);
     return;
 end
 
+% Begin with full step
+alpha = 1;
+pk = xk - params.x0;
+
+% Get duals
 lam_xk = lk(1:length(xk));
 lam_gk = lk((length(xk)+1):end);
 
-alpha = 1;
-pk = 1;
-
-i = 0;
-% Inner SQP loop
+% Main Loop
 while ( true )        
     i = i + 1;
 
@@ -218,7 +223,7 @@ while ( true )
         i = 0;
         Rk = penaltyUpdater(Rk);
     end
-    if (i > maxIter)
+    if (iter > maxIter)
         disp('Maximum number of iterations reached. Exiting without convergence');
         
         exitflag = 2;
@@ -227,31 +232,27 @@ while ( true )
     end
     
     %% Step computation
-    d = grad_phi(xk) + (rand(nx, 1) - 0.5)*4*eps;    
+    % Update linearization
+    d = grad_phi(xk);
+    
+    % Add perturbation if desired (could help avoid spurious solutions)
+    if (useGradientPerturbation)
+         d = d + (rand(nx, 1) - 0.5)*4*eps;
+    end
 
+    % Solve convex QP
     [x_new,~,exitflag,iterQPO,lambda,auxOutput] = qpOASES_sequence('h', QP, g + Rk*d, lb, ub, lbA, ubA, options);      
     
+    % Terminate if qpOASES fails
     if (exitflag ~= 0)
         qpOASES_sequence('c', QP);
         return;
     end
 
-    %% Otpimal step length computation
+    %% Step length computation
     pk = x_new - xk;
     alpha = GetOptimalStepLength(C, g, Rk, Q, pk, xk);
-    
-    % Handle Optimal 0 step
-    if (alpha == 0)
-        if (norm(pk) < stationarityTolerance || phi(xk) < complementarityTolerance)
-            alpha = 1;
-        else
-            % Update penalty parameter
-            k = k + 1;
-            i = 0;
-            Rk = penaltyUpdater(Rk);
-        end
-    end
-    
+        
     %% Step update
     xk = xk + alpha*pk;
     lam_xk = lambda(1:nx);
